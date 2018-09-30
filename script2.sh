8d7a1fc56e0fccfdc7c4ec669bc12bc640b2d33d
Raj Kiran Gade <raj@redlock.io>
2018-09-30 20:52:37 +0530

    changed this

--- script2.sh
+++ script2.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-$1
-$2
-
+# Check if there are two parameters, else quit.
+if [[ "$#" -ne 2 ]]; then
+echo "Pass two parameters"
+exit 127
+fi
+: '
 c=$(tar -cvf backup.tar -g snapshot.snar-0 $1)
 echo 
 g=$(gzip backup.tar | mv backup.tar.gz $2 )
 echo
 echo $#
+ '
+
+echo "Changed This"
